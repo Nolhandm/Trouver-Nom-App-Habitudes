d6a53efb9bd55c5c2b5f8f6e175c3198945dc85f
Nolhandm <nolhan.dumoulin@gmail.com>
2026-07-06 23:15:18 +0200

    added main and pages

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+import streamlit as st
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+st.set_page_config(page_title="Application Habitudes", page_icon="⚔️")
 
+st.title("⚔️ Tableau de Bord des Habitudes")
+st.write("Bienvenue, Héros ! Coche tes quêtes pour gagner de l'XP.")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Vos logique de jeu ici
+if st.button("Valider la quête du jour"):
+    st.success("Quête validée ! +50 XP")
+    # Ici vous appelleriez votre fonction pour mettre à jour la DB
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+st.info("Utilise le menu à gauche pour naviguer vers la Boutique ou ton Profil.")
