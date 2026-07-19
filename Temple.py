427fb6c1087b9240e29c6f4fc6852b391ce0c4fb
Nolhandm <nolhan.dumoulin@gmail.com>
2026-07-19 15:51:20 +0200

    added assets

--- Temple.py
+++ Temple.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from db.init_db import init_db
-from services.statsServices import get_actual_xp
+from services.statsServices import get_total_xp, compute_level
+from utils.test import test_app
 
 init_db()
 
@@ -9,4 +10,7 @@ st.set_page_config(page_title="Application Habitudes", page_icon="⚔️")
 st.title("⚔️ Tableau de Bord des Habitudes")
 st.write("Bienvenue, Héros ! Coche tes quêtes pour gagner de l'XP.")
 
-st.write(f"XP actuel : {get_actual_xp()}")
+st.write(f"XP actuel : {get_total_xp()}")
+st.write(f"Niveau actuel : {compute_level()}")
+
+test_app()
--- services/statsServices.py
+++ services/statsServices.py
@@ -2,10 +2,13 @@ from sqlalchemy import func
 from sqlmodel import Session, select
 from db.init_db import get_engine
 from db.models import *
+from math import sqrt, pow
 
 base_xp = 10
+base_level = 1000
+increase_level = 100
 
-def get_actual_xp():
+def get_total_xp():
     with Session(get_engine()) as session:
 
         # Get number of checks for each habit
@@ -18,4 +21,11 @@ def get_actual_xp():
         for hab in result:
             final_xp += base_xp*hab[1]*(hab[0].time_coeff+hab[0].difficulty_coeff+hab[0].importance_coeff)
         return final_xp
+
+def compute_level():
+
+    cumul = get_total_xp()
+    level = (increase_level/2 - base_level + sqrt(pow(base_level-increase_level/2,2)+2*increase_level*cumul))/increase_level+1
+
+    return int(level)
 
